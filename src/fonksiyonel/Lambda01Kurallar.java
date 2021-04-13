e8154ff2d84967684d32339d3665b561e08675cf
Muzaffer <muzafferuste.ga@gmail.com>
2021-04-13 23:40:55 +0300

    guncelleme 13.04.2021

--- src/fonksiyonel/Lambda01Kurallar.java
+++ src/fonksiyonel/Lambda01Kurallar.java
@@ -7,9 +7,51 @@ public class Lambda01Kurallar {
 
     public static void main(String[] args) {
 
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> liste = Arrays.asList(1, 2, 3, 4, 5);
 
-        list.forEach(x -> System.out.println(x));
+        // forEach() bir collection'nın iterasyonu için kullanılan HOF'dur.
+        // Parametre olarak bir lambda fonksiyonu alabilir.
+
+        // Dizinin Elemanlarını yazdıralım
+        System.out.println("=== Dizinin Elamanlarını Yazdır ===");
+        liste.forEach((x) -> System.out.print(x));
+
+        //Listenin elemanlarını 2 arttırarak yazdıralım.
+        System.out.println("\n == Dizinin Elamanlarını 2 arttırarak Yazdır ==");
+        liste.forEach(t -> System.out.print(t + 2 + " "));
+
+        System.out.println("\n=== Eğer satır sayısı 1'den fazla ise {} kullanmalıyız..=== ");
+        liste.forEach(t -> {
+            int miktar = 2;
+            System.out.print(t + miktar + " ");
+        });
+
+        System.out.println("\n === Veri tipi kullanılırsa : Explicit ==");
+        System.out.println("Dizinin Elemanlarının 2 katını yazdır.");
+
+        liste.forEach((Integer x) -> System.out.print(x * 2 + " "));
+
+        System.out.println("\nLambda ifadesinde dış değişkenler kullanalıbilir");
+        System.out.println("Ancak bu değişken Final gibi davranır.");
+
+        int deger = 6;
+
+        liste.forEach(t -> System.out.println(t + deger));
+
+        // Metot Refransı =====> Class adı :: Metot adı
+        System.out.println("\nJavanın Method referansını kullanabiliriz");
+        liste.forEach(System.out::print);
+
+        // Metot Referansı =====> Class adı :: Metot adı
+        System.out.println("\nKendimizin yazdığı metotları da Method referansı olarak kullanabiliriz");
+        liste.forEach(Lambda01Kurallar::yazdır);
+
+    }
+
+    // int değer yazdıran metot
+    public static void yazdır(int x) {
+
+        System.out.print(x + " ");
 
     }
 
