28519f6d994e9ecd0cae5c37eca77b24afb34f39
Muzaffer <muzafferuste.ga@gmail.com>
2021-04-15 06:10:17 +0300

    Stream02Integer class'i eklendi

--- src/streamOrnekler/Stream01Integer.java
+++ src/streamOrnekler/Stream01Integer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Stream01Integer {
+
     public static void main(String[] args) {
 
         List<Integer> rakamlar = new ArrayList<>();
@@ -25,13 +26,27 @@ public class Stream01Integer {
         rakamlar.stream().filter(t -> t % 2 != 0).forEach(t -> System.out.print(t + " "));
 
         System.out.println("\nCift sayilar: ");
-        rakamlar.stream().filter( t -> t % 2 == 0).forEach(Stream01Integer::yazdir);
+        rakamlar.stream().filter(t -> t % 2 == 0).forEach(Stream01Integer::yazdir);
 
         System.out.println("\nKendi methodumuz ile filtreleme yapmadan yazdirma: ");
         rakamlar.forEach(Stream01Integer::yazdir);
 
         System.out.println("\nKendi methodumuz ile tek sayilari filtreleme ve yazdirma: ");
         rakamlar.stream().filter(Stream01Integer::tekMi).forEach(Stream01Integer::yazdir);
+
+
+        System.out.println("\n ==== CIFT SAYILAR(METHOD) ====");
+        ciftleriYazdir(rakamlar);
+
+    }
+
+    public static void ciftleriYazdir(List<Integer> list) {
+
+        // Method referansi kullanmadan rakamlar listesindeki cift sayilari yazdirma:
+        // list.stream().filter(x -> x % 2 == 0).forEach(x -> System.out.print(x + " "));
+
+        // Method referansi kullanarak rakamlar listesindeki cift sayilari yazdirma:
+        list.stream().filter(Stream01Integer::ciftMi).forEach(Stream01Integer::yazdir);
 
     }
 
@@ -41,6 +56,10 @@ public class Stream01Integer {
 
     public static boolean tekMi(int x) {
         return x % 2 != 0;
+    }
+
+    public static boolean ciftMi(int x) {
+        return x % 2 == 0;
     }
 
 }
